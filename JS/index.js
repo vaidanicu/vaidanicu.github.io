fdcf68b09bbe6d6af7588b85be8567759fa680f2
Vaida Aurel <Vaida_Aurel@yahoo.com>
2022-12-29 12:11:10 +0200

    Licht

--- JS/index.js
+++ JS/index.js
@@ -4,18 +4,30 @@ var ButtonTurnOff = document.getElementById("TurnOf");
 var TurnOfPhoto = document.getElementById("TurnOfPhoto");
 
 ButtonTurnOff.addEventListener("click", function () {
-  TurnOfPhoto.style.display = "block";
+  TurnOfPhoto.style.display = "inline";
   TurnOnPhoto.style.display = "none";
 });
 
 ButtonTurnOn.addEventListener("click", function () {
-  TurnOnPhoto.style.display = "block";
+  TurnOnPhoto.style.display = "inline";
   TurnOfPhoto.style.display = "none";
 });
 
-let activePage = "home";
+// Menu TopBar var
+var Home = document.getElementsByClassName("Home");
+var skillLi = document.getElementById("skills-list");
 
-var home = document.getElementById("Home");
-var Skills = document.getElementById("Skills");
-var projects = document.getElementById("Projects");
-var Language = document.getElementById("Language");
+//  Skills-List
+
+var skills = ["HTML", "CSS", "JavaScript"];
+var skillsHTML = "";
+var i = 0;
+for (var i = 0; i < skills.length; i++) {
+  skillsHTML += "<li>" + skills[i] + "</li>";
+}
+skillLi.innerHTML = skillsHTML;
+
+document.addEventListener("click", function () {
+  Home.style.display = "none";
+  skillLi.style.display = "inline";
+});

6d428a35fcf6909cf8a52b12440f65c39f06a52f
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-01-19 21:40:26 +0200

    replace string concatention with string template

--- JS/index.js
+++ JS/index.js
@@ -10,7 +10,7 @@ var skills = [
 ];
 
 var skillsHTML = skills.map(function (skill) {
-  return "<li>" + skill.name + "-" + skill.endorcements + "</li>";
+  return `<li>${skill.name} - <span>${skill.endorcements}</span></li>`;
 });
 
 skillsEl.innerHTML = skillsHTML.join("");

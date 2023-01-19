a7324d13af1e2dd331218ea6c57da80e2b387846
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-01-19 21:27:44 +0200

    replace forEach with map to create skills html

--- JS/index.js
+++ JS/index.js
@@ -9,13 +9,11 @@ var skills = [
   { name: "Drive", endorcements: 2 },
 ];
 
-var skillsHTML = "";
-
-skills.forEach(function (skill) {
-  skillsHTML += "<li>" + skill.name + "-" + skill.endorcements + "</li>";
+var skillsHTML = skills.map(function (skill) {
+  return "<li>" + skill.name + "-" + skill.endorcements + "</li>";
 });
 
-skillsEl.innerHTML = skillsHTML;
+skillsEl.innerHTML = skillsHTML.join("");
 
 function show(id) {
   document.getElementById(id).style.display = "block";

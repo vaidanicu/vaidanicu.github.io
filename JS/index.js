194c744bdd136a8bfcec60aa3ad0db650a95966e
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-01-24 11:02:31 +0200

    add Views

--- JS/index.js
+++ JS/index.js
@@ -103,3 +103,15 @@ function DisplayDate() {
 DisplayDate();
 
 setInterval(DisplayDate, 3000);
+
+const countEl = document.getElementById("count");
+
+updateVisitCount();
+
+function updateVisitCount() {
+  fetch("https://api.countapi.xyz/update/laptop/Aurel/?amount=1")
+    .then((res) => res.json())
+    .then((res) => {
+      countEl.innerHTML = res.value;
+    });
+}
